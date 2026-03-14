e7532390f6487598194295f9dba96faf53aac131
José Gabriel González <jgg032899@gmail.com>
2026-03-14 16:44:22 -0400

    Added two simple tests

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -1,11 +1,25 @@
 import { describe, it, expect } from 'vitest'
-
 import { mount } from '@vue/test-utils'
 import App from '../App.vue'
 
 describe('App', () => {
-  it('mounts renders properly', () => {
+  it('calculates correct send amount and fees for 831.33 to receive', async () => {
     const wrapper = mount(App)
-    expect(wrapper.text()).toContain('You did it!')
+
+    const input = wrapper.find('input[placeholder="0.00"]')
+    await input.setValue(831.33)
+
+    expect(wrapper.text()).toContain('$879.10')
+    expect(wrapper.text()).toContain('-$47.77')
+  })
+
+  it('calculates correct send amount and fees for 1000 to receive', async () => {
+    const wrapper = mount(App)
+
+    const input = wrapper.find('input[placeholder="0.00"]')
+    await input.setValue(1000)
+
+    expect(wrapper.text()).toContain('$1057.40')
+    expect(wrapper.text()).toContain('-$57.40')
   })
 })
